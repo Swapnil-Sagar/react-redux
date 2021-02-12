102655152bef24d80e8cc16dfb62c8b706242809
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-02-12 14:04:09 +0530

    mapStateToProps

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import IceCreamContainer from "./components/IceCreamContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import HooksIceCreamContainer from "./components/HooksIceCreamContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <CakeContainer />
         <HooksCakeContainer />
         <NewCakeContainer />
